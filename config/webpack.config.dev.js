0f5636187e7430b1ef2a814a38a792260c4ae73e
Marcio Gasparotto <mgasparotto@gmail.com>
2017-07-22 02:27:59 +0000

    eject and config decorator

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -68,8 +68,7 @@ module.exports = {
     // This is the URL that app is served from. We use "/" in development.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location
-    devtoolModuleFilenameTemplate: info =>
-      path.resolve(info.absoluteResourcePath),
+    devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath),
   },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
@@ -86,7 +85,6 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -132,16 +130,7 @@ module.exports = {
       // When you `import` an asset, you get its (virtual) filename.
       // In production, they would get copied to the `build` folder.
       {
-        exclude: [
-          /\.html$/,
-          /\.(js|jsx)$/,
-          /\.css$/,
-          /\.json$/,
-          /\.bmp$/,
-          /\.gif$/,
-          /\.jpe?g$/,
-          /\.png$/,
-        ],
+        exclude: [/\.html$/, /\.(js|jsx)$/, /\.css$/, /\.json$/, /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
         loader: require.resolve('file-loader'),
         options: {
           name: 'static/media/[name].[hash:8].[ext]',
@@ -163,13 +152,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
-        options: {
 
+        options: {
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
           cacheDirectory: true,
           plugins: ['transform-decorators-legacy'],
+          // presets: ['stage-1'],
         },
       },
       // "postcss" loader applies autoprefixer to our CSS.
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,72 +3,71 @@ import { Link } from 'react-router-dom';
 import Sanfona from '../sanfona/sanfona';
 import { observer } from 'mobx-react';
 
-const SideBar = observer(
-  class Sidebar extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-
-    render() {
-      const { sidebarStore } = this.props;
-      return (
-        <aside className="layout-sidebar sv-column">
-          <div className="logo">
-            <img src={`${process.env.PUBLIC_URL}images/saturn-v.svg`} width="150" height="auto" />
-          </div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/colors">Cores</Link></li>
-            <li><Link to="/grid">Grid</Link></li>
-            <li><Link to="/espacamentos">Espaçamentos</Link></li>
-            <li>
-              <Sanfona
-                title="Botões"
-                stateKey="buttonsOpened"
-                toggle={() => sidebarStore.toggle('buttonsOpened')}
-                opened={sidebarStore.sanfonas['buttonsOpened']}
-              >
-                <ul>
-                  <li><Link to="/buttons/sobre">Padrão</Link></li>
-                  <li><Link to="/buttons/tamanhos">Tamanhos</Link></li>
-                  <li><Link to="/buttons/especiais">Especiais</Link></li>
-                </ul>
-              </Sanfona>
-            </li>
-            <li>
-              <Sanfona
-                title="Formulários"
-                stateKey="formOpened"
-                toggle={() => sidebarStore.toggle('formOpened')}
-                opened={sidebarStore.sanfonas['formOpened']}
-              >
-                <ul>
-                  <li><Link to="/formularios/inputs">Inputs</Link></li>
-                  <li><Link to="/formularios/layouts">Layouts</Link></li>
-                </ul>
-              </Sanfona>
-            </li>
-            <li>
-              <Sanfona
-                title="Componentes"
-                stateKey="componentsOpened"
-                toggle={() => sidebarStore.toggle('componentsOpened')}
-                opened={sidebarStore.sanfonas['componentsOpened']}
-              >
-                <ul>
-                  <li><Link to="/componentes/breadcrumbs">Breadcrumbs</Link></li>
-                  <li><Link to="/componentes/loaders">Loaders</Link></li>
-                  <li><Link to="/componentes/messagebox">Message box</Link></li>
-                  <li><Link to="/componentes/tags">Tags</Link></li>
-                </ul>
-              </Sanfona>
-            </li>
-
-          </ul>
-        </aside>
-      );
-    }
+@observer
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
   }
-);
+
+  render() {
+    const { sidebarStore } = this.props;
+    return (
+      <aside className="layout-sidebar sv-column">
+        <div className="logo">
+          <img src={`${process.env.PUBLIC_URL}images/saturn-v.svg`} width="150" height="auto" />
+        </div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/colors">Cores</Link></li>
+          <li><Link to="/grid">Grid</Link></li>
+          <li><Link to="/espacamentos">Espaçamentos</Link></li>
+          <li>
+            <Sanfona
+              title="Botões"
+              stateKey="buttonsOpened"
+              toggle={() => sidebarStore.toggle('buttonsOpened')}
+              opened={sidebarStore.sanfonas['buttonsOpened']}
+            >
+              <ul>
+                <li><Link to="/buttons/sobre">Padrão</Link></li>
+                <li><Link to="/buttons/tamanhos">Tamanhos</Link></li>
+                <li><Link to="/buttons/especiais">Especiais</Link></li>
+              </ul>
+            </Sanfona>
+          </li>
+          <li>
+            <Sanfona
+              title="Formulários"
+              stateKey="formOpened"
+              toggle={() => sidebarStore.toggle('formOpened')}
+              opened={sidebarStore.sanfonas['formOpened']}
+            >
+              <ul>
+                <li><Link to="/formularios/inputs">Inputs</Link></li>
+                <li><Link to="/formularios/layouts">Layouts</Link></li>
+              </ul>
+            </Sanfona>
+          </li>
+          <li>
+            <Sanfona
+              title="Componentes"
+              stateKey="componentsOpened"
+              toggle={() => sidebarStore.toggle('componentsOpened')}
+              opened={sidebarStore.sanfonas['componentsOpened']}
+            >
+              <ul>
+                <li><Link to="/componentes/breadcrumbs">Breadcrumbs</Link></li>
+                <li><Link to="/componentes/loaders">Loaders</Link></li>
+                <li><Link to="/componentes/messagebox">Message box</Link></li>
+                <li><Link to="/componentes/tags">Tags</Link></li>
+              </ul>
+            </Sanfona>
+          </li>
+
+        </ul>
+      </aside>
+    );
+  }
+}
 
 export default SideBar;
